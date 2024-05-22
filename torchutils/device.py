72bee5ef468ac55d9453ef9d3918dbcc2008093a
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2024-05-22 14:51:52 -0400

    lint

--- torchutils/device.py
+++ torchutils/device.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+'''
+torchutils.device
+
+setup torch device.
+'''
 
 import torch
 
 _DEVICE = None
 
 
-def monkeypatch_device(device: str = 'cuda', is_set_default_device: bool = True):
+def monkeypatch_device(device_str: str = 'cuda', is_set_default_device: bool = True):
     global _DEVICE
 
-    _DEVICE = torch.device(device)
+    _DEVICE = torch.device(device_str)
 
     if is_set_default_device:
         torch.set_default_device(_DEVICE)
